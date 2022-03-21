d37468d510e94960520ab9faecc0198ba011e25c
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-03-21 18:20:54 +0200

    Update QA_2.java

--- src/Boiarchuk/QA_2.java
+++ src/Boiarchuk/QA_2.java
@@ -1,2 +1,27 @@
-package Boiarchuk;public class QA_2 {
+package Boiarchuk;
+
+public class QA_2 {
+    public static void main(String[] args) {
+        int[][] array = new int[6][4];
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 200 - 100);
+            }
+        }
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        int max = array[0][0];
+        for (int i = 1; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                }
+            }
+        }
+        System.out.println("Max values: " + max);
+    }
 }
